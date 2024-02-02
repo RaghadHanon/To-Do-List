f9d0beddf356eb60dbfbd35b881c68a9b99d35db
RaghadHanon <raghadhanoon2015@gmail.com>
2024-02-02 23:45:52 +0200

    edit add function in js

--- main.js
+++ main.js
@@ -1,62 +1,3 @@
-/*const addTask=document.querySelector(".addTask");
-addTask.onsubmit = getValue;
-
-function getValue(e){
-  e.preventDefault();
-  const task=e.target.elements['Task'].value;
-  if(task!=""){
-    e.target.elements['Task'].value="";
-    add(task);
-  }
-  else alert("Empty input");
-
-}
-
-function add(task) {
-  const tasksList = document.querySelector('.tasks');
-
-  const listItem = document.createElement('li');
-  listItem.classList.add('item');
-  listItem.innerHTML = `
-      <div>
-         <input type="checkbox" name="check" class="checkBoxInput" />
-         <span>${task}</span>
-      </div>
-      <i class="delete fa-solid fa-minus" style="color: #e09996;"></i>
-   
-  `;
-
-  tasksList.appendChild(listItem);
-
-  document.querySelector('.checkBoxInput').addEventListener('change', function (e) {
-   if (e.target.classList.contains('checkBoxInput')) {
-      taskDone(e);
-    }
-  });
-  
-  tasksList.addEventListener('click', function (e) {
-    if (e.target.classList.contains('delete')) {
-      deleteTask(e);
-    }
-  });
-
-}
-
-function taskDone(e){
-  e.preventDefault();
-  //console.log( e.target.nextElementSibling);
-  //e.target.nextSibling.classList.toggle("done");
-
-  console.log(e.target.nextSibling);
-};
-  
-function deleteTask(e){
-    e.preventDefault();
-    console.log(  e.target);
-    e.target.parentElement.remove(); 
-}
-
-*/
 
 
 const addTask=document.querySelector(".addTask");
@@ -94,7 +35,7 @@ function add(task) {
 
   tasksList.appendChild(listItem);
 
-  checkDiv.addEventListener('change', function (e) {
+  listItem.addEventListener('change', function (e) {
    if (e.target.classList.contains('checkBoxInput')) {
       taskDone(e);
     }
@@ -110,10 +51,7 @@ function add(task) {
 
 function taskDone(e){
   e.preventDefault();
-  //console.log( e.target.nextElementSibling);
   e.target.nextElementSibling.classList.toggle("done");
-
-  console.log(e.target.nextElementSibling);
 };
   
 function deleteTask(e){
@@ -121,9 +59,6 @@ function deleteTask(e){
     console.log(  e.target);
     e.target.parentElement.remove(); 
 }
-
-
-
 
 
 
